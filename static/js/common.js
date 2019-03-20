a5b7fd6dd67a54b7c026b42411d3f8dba9ff0806
qjwvtd <176156562@qq.com>
2019-03-20 21:53:26 +0800

    'addimg'

--- static/js/common.js
+++ static/js/common.js
@@ -1,11 +1,11 @@
 /** 
  * 设置导航active class="<%= isMainActive("/") %>">
  */
-exports.mainActive = function (req, res, next) {
-    res.locals.isMainActive = function (nav) {
-        let currentPath = req.path;
-        let activeClass = nav == currentPath ? 'active' : '';
-        return activeClass;
+exports.mainActive = (req,res,next) => {
+    res.locals.isMainActive = (path) => {
+        const CURRENTPATH = req.path;
+        const ACTIVE = path == CURRENTPATH ? 'active' : '';
+        return ACTIVE;
     };
     next();
-};
+}
